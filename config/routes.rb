2b45ba71e9b3e93b989e5a8f235fb46939e11183
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-03-13 11:02:39 +0100

    scaffold instruments & remove store

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :instruments
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  root to: "store#index"
+  root to: "instruments#index"
 end
